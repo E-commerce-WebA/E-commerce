f6c8bbe9018cc0082f7cce96525e2a38377550d3
eualx <eualx14j@gmail.com>
2024-10-10 22:53:39 +0300

    -cart

--- src/components/ReactTable/ReactTable.tsx
+++ src/components/ReactTable/ReactTable.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
-
-const ReactTable = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default ReactTable
+import React from "react";
+import type { ColumnDef } from "@tanstack/react-table";
+import { flexRender, getCoreRowModel, useReactTable, getSortedRowModel } from "@tanstack/react-table";
+type ReactTableProps<D extends unknown> = {
+data: D[];
+columns: ColumnDef<D, string>[];
+headerProps?: {};
+headerCellProps?: {};
+bodyProps?: {};
+rowDataProps?: { className: string };
+cellProps?: {className: string };
+size?: keyof typeof sizes;
+[x: string]: any;
+};
+const sizes = {
+xs: "py-[30px]",
+} as const;
+const ReactTable = <D extends unknown>({
+columns,
+data = [],
+headerProps = {},
+headerCellProps = {},
+bodyProps = {},
+className="",
+rowDataProps = { className: ""},
+cellProps = { className:""},
+size,
+...restConfig
+}: ReactTableProps<D>) => {
+const tableConfig = {
+columns,
+data,
+getCoreRowModel: getCoreRowModel(),
+getSortedRowModel: getSortedRowModel(),
+...restConfig,
+};
+const table = useReactTable(tableConfig);
+// Render the UI for your table
+return (
+<table className={className}>
+<thead {...headerProps}>
+{table.getHeaderGroups().map((headerGroup) => (
+<tr key={headerGroup.id}>
+{headerGroup.headers.map((header) => (
+<th key={header.id} {...header.column.columnDef?.meta} {...headerCellProps}>
+{header.isPlaceholder? null: flexRender (header.column.columnDef.header, header.getContext())}
+</th>
+))}
+</tr>
+))}
+</thead>
+<tbody {...bodyProps}>
+{table.getRowModel().rows.map((row) => (
+<tr {...rowDataProps} className={`${rowDataProps?.className}`} key={row.id}>
+{row.getVisibleCells().map((cell) => (
+<td key={cell.id} className={`${size? sizes [size]:""} ${cellProps?.className} `}>
+{flexRender(cell.column.columnDef.cell, cell.getContext())}
+</td>
+))}
+</tr>
+))}
+</tbody>
+</table>
+);
+};
+export { ReactTable };
